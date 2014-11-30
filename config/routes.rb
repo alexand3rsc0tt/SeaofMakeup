edbc1f71455f01ebc7bacca9d7d1af1441befd94
alexand3rsc0tt <alex.burton1993@gmail.com>
2014-11-29 20:12:22 -0700

    set up posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+
   root "static#about"
   get "/contact", to: "static#contact", as: :contact
   get "/faq", to: "static#faq", as: :faq
-  get "/posts", to: "static#posts", as: :post
+  resources :posts
 end
