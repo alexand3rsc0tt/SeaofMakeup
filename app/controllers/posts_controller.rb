61d543102bdfb9b437ab34cb954b2e1799f0c74d
alexand3rsc0tt <alex.burton1993@gmail.com>
2014-11-29 21:09:34 -0700

    adding in admin things

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_admin!, :except => [:show, :index]
+  
   def index
     @posts = Post.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   get "/contact", to: "static#contact", as: :contact
   get "/faq", to: "static#faq", as: :faq
   resources :posts
+
+  authenticate :admin do
+    resources :posts, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :posts, only: [:index, :show]
 end
