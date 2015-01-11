eff2c9556f7089060c760b396b4aa458c6cf1fc6
Alex <alex@karens-mbp.attlocal.net>
2015-01-11 11:40:05 -0800

    s3

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,13 +1,4 @@
 class Ckeditor::Asset < ActiveRecord::Base
   include Ckeditor::Orm::ActiveRecord::AssetBase
   include Ckeditor::Backend::Paperclip
-
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['seaofmakeup'],
-      :access_key_id => ENV['AKIAIIU6K3UAF4PC45IQ'],
-      :secret_access_key => ENV['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']
-    }
-  }
 end
--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -3,15 +3,6 @@ class Ckeditor::AttachmentFile < Ckeditor::Asset
                     :url => "/ckeditor_assets/attachments/:id/:filename",
                     :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
 
-                    config.paperclip_defaults = {
-                      :storage => :s3,
-                      :s3_credentials => {
-                        :bucket => ENV['seaofmakeup'],
-                        :access_key_id => ENV['AKIAIIU6K3UAF4PC45IQ'],
-                        :secret_access_key => ENV['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']
-                      }
-                    }
-                    
   validates_attachment_presence :data
   validates_attachment_size :data, :less_than => 100.gigabytes
   do_not_validate_attachment_file_type :data
