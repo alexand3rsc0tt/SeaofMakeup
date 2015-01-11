1502a8c6335e1ab2156189cf11dae161ccbdb937
Alex <alex@karens-mbp.attlocal.net>
2015-01-11 11:36:24 -0800

    s3

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -4,6 +4,15 @@ class Ckeditor::Picture < Ckeditor::Asset
                     :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                     :styles => { :content => '800>', :thumb => '118x100#' }
 
+                    config.paperclip_defaults = {
+                      :storage => :s3,
+                      :s3_credentials => {
+                        :bucket => ENV['seaofmakeup'],
+                        :access_key_id => ENV['AKIAIIU6K3UAF4PC45IQ'],
+                        :secret_access_key => ENV['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']
+                      }
+                    }
+
   validates_attachment_presence :data
   validates_attachment_size :data, :less_than => 200.gigabytes
   validates_attachment_content_type :data, :content_type => /\Aimage/
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,5 @@
 Rails.application.configure do
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['seaofmakeup'],
-      :access_key_id => ENV['AKIAIIU6K3UAF4PC45IQ'],
-      :secret_access_key => ENV['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']
-    }
-  }
+
 
   # Settings specified here will take precedence over those in config/application.rb.
 
