80087efa36a6a081e05ebe6015bb6590bdb0790f
alexand3rsc0tt <alex.burton1993@gmail.com>
2015-02-20 18:47:50 -0800

    pictures

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -4,7 +4,6 @@ class Ckeditor::AttachmentFile < Ckeditor::Asset
                     :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
 
   validates_attachment_presence :data
-  validates_attachment_size :data, :less_than => 100.gigabytes
   do_not_validate_attachment_file_type :data
 
   def url_thumb
