5953307ebbe4436d496b057f4d495f8c5ae51d59
alexand3rsc0tt <alex.burton1993@gmail.com>
2015-03-03 19:09:50 -0800

    boop

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,7 +1,7 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
                     :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
+                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension"
 
   validates_attachment_presence :data
   validates_attachment_content_type :data, :content_type => /\Aimage/
